18e8c5cebe160f4f30cbbbefc2a81eed98d4fb24
qobulovasror <qobulovasror0@gmail.com>
2023-12-17 22:19:51 +0500

    add get and delete feedback api

--- controller/feedbackController.js
+++ controller/feedbackController.js
@@ -1,5 +1,15 @@
 const {Feedback, feedbackValidate} = require('../models/feedback');
 
+async function getFeedback(req, res) {
+  try {
+    const feedbacks = await Feedback.find();
+    if(!feedbacks) return res.status(404).send("data not fount");
+    res.json(feedbacks);
+  } catch (error) {
+    res.status(500).json({ error: `Data fetch error: ${error}` });
+  }
+}
+
 async function postFeedback(req, res) {
   try {
     const { error } = feedbackValidate(req.body);
@@ -10,10 +20,26 @@ async function postFeedback(req, res) {
       text: req.body.text,
     });
     feedback = await feedback.save();
-    res.send({status: 200, text: "Successful saved"});
+    res.status(200).send({text: "Successful saved"});
   } catch (error) {
     res.status(500).json({ error: `Data fetch error: ${error}` });
   }
 }
 
-module.exports = {postFeedback};
+async function deleteFeedback(req, res) {
+  try {
+    const feedBackId = req.params.id;
+    if(!feedBackId) return res.status(400).send("id not fount");
+    const feedback = await Feedback.findOneAndDelete({_id: feedBackId});
+    if(!feedback) return res.status(400).send("feedback not fount");
+    res.send({text: "Successful deleted"});
+  } catch (error) {
+    res.status(500).json({ error: `Data fetch error: ${error}` });
+  }
+}
+
+module.exports = {
+    postFeedback,
+    getFeedback,
+    deleteFeedback
+};
--- routes/feedback.js
+++ routes/feedback.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
+const auth = require('../middleware/auth');
+const { admin } = require('../middleware/role');
 const feedbackController = require('../controller/feedbackController');
 
+// get all feedbecks
+router.get('/', auth, admin,  feedbackController.getFeedback);
 
 // add feedback
 router.post('/', feedbackController.postFeedback);
+
+//delete feedbacks
+router.delete('/:id', auth, admin, feedbackController.deleteFeedback)
 
 module.exports = router;
